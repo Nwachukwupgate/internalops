8d1dcff1b4296e2b8147eb4b4c56a1b17fae0f0e
treyp99 <nwachukwupgate99@gmail.com>
2025-06-06 08:58:08 +0100

    added scroll to pricing

--- components/Hero/index.tsx
+++ components/Hero/index.tsx
@@ -24,12 +24,15 @@ const Hero = () => {
                   and ready to scale with your organization.
                 </p>
                 <div className="flex flex-col items-center justify-center space-y-4 sm:flex-row sm:space-x-4 sm:space-y-0">
-                  <Link
-                    href="https://nextjstemplates.com/templates/saas-starter-startup"
+                  <p
+                    onClick={() => {
+                      const contactSection = document.getElementById("pricing");
+                      contactSection?.scrollIntoView({ behavior: "smooth" });
+                    }}
                     className="rounded-sm bg-primary px-8 py-4 text-base font-semibold text-white duration-300 ease-in-out hover:bg-primary/80"
                   >
                     🔥 Get Internalops Pro
-                  </Link>
+                  </p>
                 </div>
               </div>
             </div>
