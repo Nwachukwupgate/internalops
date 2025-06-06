3e6f0b51502158c45fd6241aefcee2935897a710
treyp99 <nwachukwupgate99@gmail.com>
2025-06-06 18:42:49 +0100

    updated sitemap

--- app/sitemap.xml/route.ts
+++ app/sitemap.xml/route.ts
@@ -1,6 +1,4 @@
-import { MetadataRoute } from "next";
-
-export async function GET(): Promise<MetadataRoute.Sitemap> {
+export async function GET(): Promise<Response> {
   const baseUrl = "https://internalops.pro";
 
   const routes = [
@@ -10,8 +8,26 @@ export async function GET(): Promise<MetadataRoute.Sitemap> {
     "/about",
   ];
 
-  return routes.map((route) => ({
-    url: `${baseUrl}${route}`,
-    lastModified: new Date(),
-  }));
+  const sitemap = routes
+    .map((route) => {
+      return `
+        <url>
+          <loc>${baseUrl}${route}</loc>
+          <lastmod>${new Date().toISOString()}</lastmod>
+        </url>
+      `;
+    })
+    .join("");
+
+  const xml = `<?xml version="1.0" encoding="UTF-8"?>
+    <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+      ${sitemap}
+    </urlset>
+  `;
+
+  return new Response(xml, {
+    headers: {
+      "Content-Type": "application/xml",
+    },
+  });
 }
